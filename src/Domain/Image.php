3dcf0f0b77e4d04605b49ce9750b57ea9c79cbc9
TheKiryuKha <kirillzuyeu@gmai.com>
2026-02-15 07:44:25 +0300

    feat: main entities

---
+++ src/Domain/Image.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain;
+
+final readonly class Image
+{
+    private ?int $id = null;
+    private ?string $path = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): void
+    {
+        $this->path = $path;
+    }
+}
---
+++ src/Domain/Post.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain;
+
+use App\Domain\Image;
+use App\Domain\Category;
+
+final readonly class Post
+{
+    private ?int $id = null;
+    private ?string $title = null;
+    private ?string $description = null;
+    private ?string $text = null;
+    private int $views = 0;
+    private ?Image $image = null;
+
+    /** @var Category[] $categories */
+    private array $categories = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+
+    public function getViews(): int
+    {
+        return $this->views;
+    }
+
+    public function view(): void
+    {
+        $this->views++;
+    }
+
+    public function getImage(): ?Image
+    {
+        return $this->image;
+    }
+
+    public function setImage(Image $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function addCategory(Category $category): void
+    {
+        $this->categories[] = $category;
+    }
+
+    /**
+     * @return Category[]
+     */
+    public function getCategories(): array
+    {
+        return $this->categories;
+    }
+}
