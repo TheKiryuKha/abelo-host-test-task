d0652d9d8e76301e223df49fe8c41f3c05f2c809
TheKiryuKha <kirillzuyeu@gmai.com>
2026-02-15 22:03:52 +0300

    feat: database in docker

--- public/index.php
+++ public/index.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 phpinfo();
